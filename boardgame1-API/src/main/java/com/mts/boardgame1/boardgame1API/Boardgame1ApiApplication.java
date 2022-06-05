fed91b19df22879a8d3c8a5ffd67200974fec7bc
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-06-05 15:21:12 +0300

    first

---
+++ boardgame1-API/src/main/java/com/mts/boardgame1/boardgame1API/Boardgame1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.mts.boardgame1.boardgame1API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boardgame1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boardgame1ApiApplication.class, args);
+	}
+
+}
---
+++ boardgame1-API/src/test/java/com/mts/boardgame1/boardgame1API/Boardgame1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.boardgame1.boardgame1API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boardgame1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
